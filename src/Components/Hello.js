5d106e968b4982117a58051aec92ad17f5f02f32
Talibiq123 <talibsaeed90@gmail.com>
2023-12-18 18:10:24 +0530

    useContext

--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -2,18 +2,30 @@ import React, { useContext } from "react";
 import ColorContext from "../Context/ColorContext";
 
 function Hello() {
-    const {style} = useContext(ColorContext)
+    const {style, blackMode, blueMode, whiteMode} = useContext(ColorContext)
 
     return (
         <div>
+        <div style={{
+            color: style.color,
+            backgroundColor: style.backgroundColor,
+            border: style.border,
+            textAlign: style.textAlign
+        }}>
             <h1>Hey, Theme Switcher is developing...Have Patience.</h1>
-            <p>
-                lorem ipsum dolor sit amet, consectetur adip lorem conv aptur et lorem conv lorem
-                conv lorem lorem ipsum dolor sit amet, consectetur adip lorem conv aptur et lorem conv lorem
-                conv lorem lorem ipsum dolor sit amet, consectetur adip lorem conv aptur et lorem conv
-                lorem conv lorem lorem ipsum dolor sit amet, consectetur adip lorem conv aptur et lorem conv lorem conv lorem
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, nisl ac aliquet euismod, eros erat varius orci, nec vestibulum odio diam
+               in justo. Integer auctor urna in velit vestibulum, nec tristique leo dapibus. Aliquam erat volutpat. Suspendisse potenti. Fusce 
+               ullamcorper sem in odio varius, vitae bibendum elit feugiat. Maecenas ut mauris at tortor cursus gravida. Quisque vel tristique 
+               purus. Vivamus aliquet purus ac metus tincidunt, nec tincidunt mauris convallis. Integer ullamcorper, velit a tincidunt vehicula,
+               metus metus tincidunt eros, id bibendum exvelit sit amet mauris. Sed dapibus scelerisque felis, eget consequat metus tempus eget. Suspendisse potenti.
             </p>
         </div>
+        <div>
+                <button onClick={whiteMode}>White Mode</button>
+                <button onClick={blackMode}>Dark Mode</button>
+                <button onClick={blueMode}>Blue Mode</button>
+            </div>
+            </div>
     )
 }
 
--- src/Context/ColorProvider.js
+++ src/Context/ColorProvider.js
@@ -1,19 +1,38 @@
 import React, { useState } from "react";
 import ColorContext from "./ColorContext";
 
+
+let defaultStyle = {
+    color: "black",
+    backgroundColor: "white",
+    border: "1px solid black",
+    fontFamily: "Helvetica"
+}
 function ColorProvider(props) {
-    let [style, setStyle] = useState({
-        color: "black",
-        backgroundColor: "white",
-        border: "1px solid black"
-    });
+    let [style, setStyle] = useState(defaultStyle);
 
     return (
-        <div>
-            <ColorContext.Provider value={{style: style}}>
+            <ColorContext.Provider value={
+                {
+                    style: style,
+                    blackMode: () => setStyle({...style,
+                        color: "white",
+                        backgroundColor: "black",
+                        border: "1px solid white",
+                        textAlign: "center"
+                    }),
+                    blueMode: () => setStyle({...style,
+                        color: "green",
+                        backgroundColor: "#F5F5DC",
+                        border: "1px solid green",
+                        textAlign: "right"
+                    }),
+                    whiteMode: () => setStyle(defaultStyle
+                    ),
+                }
+            }>
                 {props.children}
             </ColorContext.Provider>
-        </div>
     );
 }
 
